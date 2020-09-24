98e2e1d293af4dc2b320869fe8b8af83cac2066c
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-09-24 17:39:05 -0300

    crear componente header y logo

--- src/view/Header.js
+++ src/view/Header.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import LogoXL from "./LogoXL";
 
 const Header = () => {
-    return(
-        <div>
-        <h1>Hola Mundo!</h1>
-        </div>
-    )
-}
+  return (
+    <div className= "containerHeader">
+      <LogoXL/>
+      <div className="navbar">
+        <nav>
+          <a href="">  Red solidaria </a>
+          <a href="">  Cómo funciona </a>
+          <a href="">  Impacto de la red </a>
+          <a href="">  2030 para todos </a>
+          <a href="">  Se parte de la red </a>
+        </nav>
+      </div>
+      <div className="BtnDonar">
+          <button>Dona un almuerzo</button>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
---
+++ src/view/LogoXL.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LogoXL = () => {
+    return(
+        
+        <div className= "LogoXL">
+            <img src= ""></img> 
+        </div>
+        
+    );
+};
+
+export default LogoXL;
