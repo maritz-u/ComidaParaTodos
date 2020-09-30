f5f191fb94dbf411a99797d6e3d6d1052a74fbed
Fabiane Santos <fanutri1@gmail.com>
2020-09-30 10:07:34 -0300

    se agrega archivo HomeProposito.js y css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,51 +14,64 @@ const Header = () => {
         <BigLogo />
         <div className={styles.navbar}>
           <nav>
-            <select className={styles.alternativas}>
-            <option className={styles.opciones} href="">  Red solidaria </option>
-            <option className={styles.opciones} href="">  Nos preocupa</option>
-            <option className={styles.opciones} href="">  Hambre cero (ODS) </option>
-            <option className={styles.opciones} href="">  Estado actual del país </option>
-            <option className={styles.opciones} href="">  Nuestro propósito </option>
-            <option className={styles.opciones} href="">  Qué buscamos </option>
-            <option className={styles.opciones} href="">  Qué fomentamos </option>
-            <option className={styles.opciones} href="">  Quién recibe la ayuda </option>
-            <option className={styles.opciones} href="">  Colaboradores </option>
-            </select>
             
-            <select className={styles.alternativas}>
-            <option className={styles.opciones} href="">  Cómo funciona </option>
-            <option className={styles.opciones} href="">  Nuestros proyectos </option>
-            <option className={styles.opciones} href="">  Modelo campaña solidaria </option>
-            <option className={styles.opciones} href="">  Modelo escuela para todos </option>
-            <option className={styles.opciones} href="">  Testimonio </option>
-            <option className={styles.opciones} href="">  Campaña solidaria </option>
-            </select>
-            
-            <select className={styles.alternativas}>
-            <option href=""> Campañas </option>
-            </select>
+              <select className={styles.alternativas}>
+                <option className={styles.opciones} href="">  Red solidaria </option>
+                <option className={styles.opciones} href="">  Nos preocupa</option>
+                <option className={styles.opciones} href="">  Situación país</option>
+                <option className={styles.opciones} href="">  Nuestra receta </option>
+                <option className={styles.opciones} href="">  Quién recibe la ayuda  </option>
+                <option className={styles.opciones} href="">  Proyectos</option>
+                <option className={styles.opciones} href="">  Alianzas </option>
+              </select>
+        
 
-            
-            <select className={styles.alternativas}>
-            <option href="">  Escuela para todos </option>
-            <option className={styles.opciones} href="">  Objetivo </option>
-            <option className={styles.opciones} href="">  Cómo funciona </option>
-            <option className={styles.opciones} href="">  Banco de alimentos </option>
-            <option className={styles.opciones} href="">  Escuelas </option>
-            <option className={styles.opciones} href="">  Cómo puedo aportar </option>
-            </select>
+         
+              <select className={styles.alternativas}>
+                <option href=""> Campañas </option>
+                <option href=""> Campaña de emergencia </option>
+                <option href=""> Navidad para todos </option>
+                <option href=""> Año nuevo para todos </option>
+              </select>
+          
 
-            
-            <select className={styles.alternativas}>
-            <option href=""> Súmate </option></select>
+           
+              <select className={styles.alternativas}>
+                <option className={styles.opciones} href="">  Proyectos </option>
+                <option className={styles.opciones} href="">  Banco de alimentos </option>
+                <option className={styles.opciones} href="">  Escuela de comida para todos </option>
+              </select>
+        
+
+
+
+           
+              <select className={styles.alternativas}>
+                <option className={styles.opciones} href="">  Testimonios </option>
+                <option className={styles.opciones} href="">  Testimonios </option>
+                <option className={styles.opciones} href="">  Prensa </option>
+                <option className={styles.opciones} href="">  Registro del impacto </option>
+              </select>
+    
+
+          
+              <select className={styles.alternativas}>
+                <option className={styles.opciones} href=""> Súmate </option>
+                <option className={styles.opciones} href=""> Soy una empresa pública/privada </option>
+                <option className={styles.opciones} href=""> Soy transportista </option>
+                <option className={styles.opciones} href=""> Soy un restaurant </option>
+                <option className={styles.opciones} href=""> Soy productor</option>
+                <option className={styles.opciones} href=""> Vivo en un sector vulnerable</option>
+
+              </select>
+          
           </nav>
         </div>
         <div className={styles.SocialContainer}>
-         <img src={FacebookC} className={styles.SocialIcon} alt= "Facebook"></img>
-         <img src={InstagramC} className={styles.SocialIcon} alt= "Instagram"></img>
-         <img src={TwitterC} className={styles.SocialIcon} alt= "Twitter"></img>
-         <img src={mailC} className={styles.SocialIcon} alt= "Mail"></img> 
+          <img src={FacebookC} className={styles.SocialIcon} alt="Facebook"></img>
+          <img src={InstagramC} className={styles.SocialIcon} alt="Instagram"></img>
+          <img src={TwitterC} className={styles.SocialIcon} alt="Twitter"></img>
+          <img src={mailC} className={styles.SocialIcon} alt="Mail"></img>
         </div>
         <div>
           <button className={styles.BtnDonar}>
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,24 +3,28 @@ import BotonFlotante from "../Components/BotonFlotante";
 import styles from "../Views/Home.module.css";
 import Header from '../Components/Header';
 import Flecha from "../Components/Flecha";
+import Proposito from "./HomeProposito";
 
 const Home = () => {
   return (
-    
-    <div className={styles.containerFirstView}>
-      <Header/>
-      <div className={styles.boxtextprincipal}>
-        <h3> La mejor receta para un mejor país </h3>
+    <div>
+      <div className={styles.containerFirstView}>
+        <Header />
+        <div className={styles.boxtextprincipal}>
+          <h3> La mejor receta para un mejor país </h3>
+        </div>
+        <div className={styles.boxtextprincipal}>
+          <h1>SOMOS COMIDA PARA TODOS</h1>
+          <h1>Y TE INVITAMOS A COMBATIR</h1>
+          <h1>EL HAMBRE EN CHILE</h1>
+        </div>
+        <BotonFlotante />
+        <Flecha />
+
       </div>
-      <div className={styles.boxtextprincipal}>
-        <h1>SOMOS COMIDA PARA TODOS</h1> 
-        <h1>Y TE INVITAMOS A COMBATIR</h1> 
-        <h1>EL HAMBRE EN CHILE</h1>
-      </div>
-      <BotonFlotante />
-      <Flecha />
+      <Proposito />
     </div>
-    
+
   );
 };
 
